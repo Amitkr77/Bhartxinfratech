68d729fe3d3a347bf4cee436d1812032a0f34390
ayushkumar21161 <ayush@homeasy.io>
2026-03-16 16:24:10 +0530

    Updated project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,6 +23,7 @@ return (
 
 <div className="w-18 h-18 white-gradient rounded-sm flex items-center justify-center mr-3">
             <img
+              loading="lazy"
               src="/logo.webp"
               alt="Bharatx Logo"
               className="w-18 h-18 rounded-sm object-contain"
